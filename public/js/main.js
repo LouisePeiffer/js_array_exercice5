cdcd1430ea46f27c3e2e27031b60443c632b9120
Louise <louisepeiffer@hotmail.com>
2021-01-04 08:43:52 +0100

    Exos 1 à 7

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+// Exo 1
+let variable1 = 'salut'
+let tabExo = ['string1', 2, 5, variable1, Boolean([4])]
+console.log(tabExo);
+
+// Exo 2
+console.log(tabExo.length);
+
+//Exo 3
+tabExo.pop()
+console.log(tabExo);
+
+//Exo 4
+tabExo.shift()
+console.log(tabExo);
+
+//Exo 5
+tabExo.push('Joyeux', 'Lundi')
+console.log(tabExo);
+
+//Exo 6
+tabExo.unshift('Bon', 'Weekend', 'les amis')
+console.log(tabExo);
+
+//Exo 7
+tabExo.reverse()
+console.log(tabExo);
+
